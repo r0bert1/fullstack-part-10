6e2ad554a887080b590e34f54a351cd4779009ec
Robert Pakkanen <rpakkane@gmail.com>
2025-08-23 21:10:15 +0300

    Improve repository list look

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,23 +1,85 @@
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  flexContainerRow: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  flexContainer: {
+    display: 'flex',
+    gap: 10,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+  data: {
+    marginLeft: 20,
+    marginBottom: 5,
+    flex: 1,
+  },
+  stats: {
+    marginTop: 15,
+    justifyContent: 'space-around',
+  },
+  stat: {
+    alignItems: 'center',
+  },
+  badge: {
+    backgroundColor: theme.colors.primary,
+    color: theme.colors.secondary,
+    alignSelf: 'flex-start',
+    padding: 4,
+    borderRadius: 4,
+  }
+});
 
 const RepositoryItem = ({ repository }) => {
   const repositoryData = {
-    "Full name": repository.fullName,
-    "Description": repository.description,
-    "Language": repository.language,
     "Stars": repository.stargazersCount,
     "Forks": repository.forksCount,
     "Reviews": repository.reviewCount,
     "Rating": repository.ratingAverage,
   }
 
+  const formatNumber = number => {
+    return number >= 1000 ? (number / 1000).toFixed(1) + 'k' : number;
+  }
+
   return (
-    <View key={repository.id}>
-      {Object.entries(repositoryData).map(([key, value]) => (
-        <Text key={key}>
-          {key}: {value}
-        </Text>
-      ))}
+    <View key={repository.id} style={styles.container}>
+      <View style={styles.flexContainerRow}>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: repository.ownerAvatarUrl,
+          }}
+        />
+        <View style={[ styles.flexContainer, styles.data ]}>
+          <Text fontWeight="bold">{repository.fullName}</Text>
+          <Text color="textSecondary">{repository.description}</Text>
+          <Text style={styles.badge}>{repository.language}</Text>
+        </View>
+      </View>
+      <View style={[ styles.flexContainerRow, styles.stats ]}>
+        {Object.entries(repositoryData).map(([key, value]) => (
+          <View style={[ styles.flexContainer, styles.stat ]} key={key}>
+            <Text fontWeight="bold">
+              {formatNumber(value)}
+            </Text>
+            <Text color="textSecondary">
+              {key}
+            </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
